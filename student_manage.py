d31b2f3677471d5fb8991426485b6d7004a2b913
Jonathan Tinoy <80259364+jonathan2019-6184@users.noreply.github.com>
2021-06-25 21:48:20 +0800

    Update student_manage.py

--- student_manage.py
+++ student_manage.py
@@ -10,26 +10,23 @@ def connection():
 
 
 def verifier():
-    a=b=c=d=e=f=0
+    a=b=c=d=e=0
     if not student_name.get():
-        t1.insert(END,"<>Student name is required<>\n")
+        t1.insert(END,"<>Student_name is required<>\n")
         a=1
     if not roll_no.get():
-        t1.insert(END,"<>Roll no is required<>\n")
+        t1.insert(END,"<>School_Id is required<>\n")
         b=1
     if not branch.get():
-        t1.insert(END,"<>Branch is required<>\n")
+        t1.insert(END,"<>Course is required<>\n")
         c=1
     if not phone.get():
-        t1.insert(END,"<>Phone number is requrired<>\n")
+        t1.insert(END,"<>Year_Level is requrired<>\n")
         d=1
     if not father.get():
-        t1.insert(END,"<>Father name is required<>\n")
+        t1.insert(END,"<>Gender is required<>\n")
         e=1
-    if not address.get():
-        t1.insert(END,"<>Address is Required<>\n")
-        f=1
-    if a==1 or b==1 or c==1 or d==1 or e==1 or f==1:
+    if a==1 or b==1 or c==1 or d==1 or e==1:
         return 1
     else:
         return 0
@@ -40,14 +37,14 @@ def add_student():
             if ret==0:
                 conn=connection()
                 cur=conn.cursor()
-                cur.execute("CREATE TABLE IF NOT EXISTS STUDENTS(NAME TEXT,ROLL_NO INTEGER,BRANCH TEXT,PHONE_NO INTEGER,FATHER TEXT,ADDRESS TEXT)")
-                cur.execute("insert into STUDENTS values(?,?,?,?,?,?)",(student_name.get(),int(roll_no.get()),branch.get(),int(phone.get()),father.get(),address.get()))
+                cur.execute("CREATE TABLE IF NOT EXISTS STUDENTS(STUDENT_AME TEXT,SCHOOL_ID INTEGER,COURSE TEXT,YEAR_LEVEL INTEGER,GENDER TEXT)")
+                cur.execute("insert into STUDENTS values(?,?,?,?,?,?)",(student_name.get(),int(school_id.get()),course.get(),int(year_level.get()),gender.get()))
                 conn.commit()
                 conn.close()
                 t1.insert(END,"ADDED SUCCESSFULLY\n")
 
 
-def view_student():
+def View_Student():
     conn=connection()
     cur=conn.cursor()
     cur.execute("select * from STUDENTS")
@@ -62,7 +59,7 @@ def delete_student():
     if ret==0:
         conn=connection()
         cur=conn.cursor()
-        cur.execute("DELETE FROM STUDENTS WHERE ROLL_NO=?",(int(roll_no.get()),))
+        cur.execute("DELETE FROM STUDENTS WHERE ROLL_NO=?",(int(school_id.get()),))
         conn.commit()
         conn.close()
         t1.insert(END,"SUCCESSFULLY DELETED THE USER\n")
@@ -72,7 +69,7 @@ def update_student():
     if ret==0:
         conn=connection()
         cur=conn.cursor()
-        cur.execute("UPDATE STUDENTS SET NAME=?,ROLL_NO=?,BRANCH=?,PHONE_NO=?,FATHER=?,ADDRESS=? where ROLL_NO=?",(student_name.get(),int(roll_no.get()),branch.get(),int(phone.get()),father.get(),address.get(),int(roll_no.get())))
+        cur.execute("UPDATE STUDENTS SET NAME=?,ROLL_NO=?,BRANCH=?,PHONE_NO=?,FATHER=?,ADDRESS=? where ROLL_NO=?",(student_name.get(),int(school_id.get()),course.get(),int(year_level.get()),gender.get(),address.get(),int(school_id.get())))
         conn.commit()
         conn.close()
         t1.insert(END,"UPDATED SUCCESSFULLY\n")
@@ -84,56 +81,47 @@ def clse():
 
 if __name__=="__main__":
     root=Tk()
-    root.title("Student Management System")
+    root.title("Event Attendance System")
      
     student_name=StringVar()
-    roll_no=StringVar()
-    branch=StringVar()
-    phone=StringVar()
-    father=StringVar()
-    address=StringVar()
+    school_id=StringVar()
+    course=StringVar()
+    year_level=StringVar()
+    gender=StringVar()
     
-    label1=Label(root,text="Student name:")
+    label1=Label(root,text="Student_name:")
     label1.place(x=0,y=0)
 
-    label2=Label(root,text="Roll no:")
+    label2=Label(root,text="School_Id:")
     label2.place(x=0,y=30)
 
-    label3=Label(root,text="Branch:")
+    label3=Label(root,text="Course:")
     label3.place(x=0,y=60)
 
-    label4=Label(root,text="Phone Number:")
+    label4=Label(root,text="Year_Level:")
     label4.place(x=0,y=90)
 
-    label5=Label(root,text="Father Name:")
+    label5=Label(root,text="Gender:")
     label5.place(x=0,y=120)
-
-    label6=Label(root,text="Address:")
-    label6.place(x=0,y=150)
 
     e1=Entry(root,textvariable=student_name)
     e1.place(x=100,y=0)
 
-    e2=Entry(root,textvariable=roll_no)
+    e2=Entry(root,textvariable=student_id)
     e2.place(x=100,y=30)
 
-    e3=Entry(root,textvariable=branch)
+    e3=Entry(root,textvariable=course)
     e3.place(x=100,y=60)
 
-    e4=Entry(root,textvariable=phone)
+    e4=Entry(root,textvariable=year_level)
     e4.place(x=100,y=90)
     
-    e5=Entry(root,textvariable=father)
+    e5=Entry(root,textvariable=gender)
     e5.place(x=100,y=120)
-
-    e6=Entry(root,textvariable=address)
-    e6.place(x=100,y=150)
     
     t1=Text(root,width=80,height=20)
     t1.grid(row=10,column=1)
    
-
-
     b1=Button(root,text="ADD STUDENT",command=add_student,width=40)
     b1.grid(row=11,column=0)
 
